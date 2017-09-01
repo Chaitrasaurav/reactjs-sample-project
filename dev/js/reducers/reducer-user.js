0e89ada8ba06a08d0afd2716c75414d934b96296
BOSTON\chaitra.naik <chaitra.naik@ef.com>
2017-09-01 12:12:13 +0530

    sample project

---
+++ dev/js/reducers/reducer-user.js
@@ -0,0 +1,20 @@
+export default function() {
+	return [
+		{
+			id: 1,
+			first: "Chaitra",
+			last: "Naik",
+			age: '25',
+			description: "Chaitra is a front end developer",
+			thumbnail: "http://lorempixel.com/100/100"
+		},
+		{
+			id: 2,
+			first: "Anubhav",
+			last: "Vishwakarma",
+			age: '27',
+			description: "Anubhav is a front end developer",
+			thumbnail: "http://lorempixel.com/100/100"
+		}
+	]
+}
